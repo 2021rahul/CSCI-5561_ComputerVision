b4503620aa92677fb257f77a45cac8838ab51ec6
Rahul Ghosh <ghosh128@umn.edu>
2019-01-31 10:39:35 -0600

    visualization upto gradients

--- HW1/SOURCE/main.m
+++ HW1/SOURCE/main.m
@@ -11,29 +11,20 @@
 % subplot(1,3,1), imshow(img), title('Input image');
 % subplot(1,3,2), imagesc(im_filtered_x), title('Differential along x direction');
 % subplot(1,3,3), imagesc(im_filtered_y), title('Differential along y direction');
-
-figure;
+% 
+% figure;
 % set(gcf, 'Position',  [100, 100, 2000, 500])
 % subplot(1,3,1), imagesc(grad_mag)
 % subplot(1,3,2), imagesc(grad_angle), colorbar
-% subplot(1,3,3), imshow(img, [], 'InitialMagnification','fit'), quiver(abs(im_filtered_x),abs(im_filtered_x));
+% img_size = size(img);
+% img_filtered_size = size(im_filtered_x);
+% [x,y] = meshgrid(1:3:img_size(2), 1:3:img_size(1));
+% subplot(1,3,3), imshow(img), hold on, q = quiver(x,y,im_filtered_y(2:3:img_filtered_size(1)-1,2:3:img_filtered_size(2)-1),im_filtered_x(2:3:img_filtered_size(1)-1,2:3:img_filtered_size(2)-1)), q.Color = 'red', q.AutoScaleFactor = 2, q.LineWidth = 2, q.ShowArrowHead = 'off';
 
-img_size = size(img);
-img_filtered_size = size(im_filtered_x);
-
-[x,y] = meshgrid(1:3:img_size(2), 1:3:img_size(1));
-set(gca,'YDir','reverse');
-quiver(x,y,im_filtered_x(2:3:img_filtered_size(1)-1,2:3:img_filtered_size(2)-1),im_filtered_y(2:3:img_filtered_size(1)-1,2:3:img_filtered_size(2)-1));
-
-% axis image %plot the quiver to see the dimensions of the plot
-% hax = gca; %get the axis handle
-% imshow(hax.XLim,hax.YLim,img); %plot the image within the axis limits
-% hold on; %enable plotting overwrite
-% 
-% quiver(x,y,im_filtered_x(2:3:img_filtered_size(1)-1,2:3:img_filtered_size(2)-1),im_filtered_y(2:3:img_filtered_size(1)-1,2:3:img_filtered_size(2)-1))
-
-
-
-
-
+figure;
+set(gcf, 'Position',  [100, 100, 2000, 500])
+subplot(1,3,1), imshow(img), title('Input image');
+grid on;
+% plot_data = ori_histo(40, 40, :);
+% bar(plot_data(:))
 
