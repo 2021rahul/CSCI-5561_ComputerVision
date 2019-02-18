3d6f5cac57cd86217917822fd0644e28a90d05f5
Rahul Ghosh <ghosh128@umn.edu>
2019-02-18 14:13:56 -0600

    data and structure update

--- HW2/SOURCE/FindMatch.m
+++ HW2/SOURCE/FindMatch.m
@@ -1,5 +1,11 @@
 function [x1, x2] = FindMatch(I1,I2)
-    x1=I1;
-    x2=I2;
+    [f1,d1] = vl_sift(single(I1));
+    [f2,d2] = vl_sift(single(I2));
+    
+    idx = knnsearch(d2',d1');
+    x1 = f1';
+    x1 = x1(:,1:2);
+    x2 = f2';
+    x2 = x2(idx,1:2);
 end
 
--- HW2/SOURCE/main.m
+++ HW2/SOURCE/main.m
@@ -1,2 +1,22 @@
+close all;
 run('~/MATLAB/vlfeat-0.9.21/toolbox/vl_setup')
-vl_version verbose
+
+image = imread("../DATA/IMAGE.png");
+image = rgb2gray(image);
+template = imread("../DATA/TEMPLATE.png");
+template = rgb2gray(template);
+
+[ximage, xtemplate] = FindMatch(image,template);
+
+
+
+
+
+
+
+% figure;
+% imshow(template);
+% perm = randperm(size(f,2)) ;
+% sel = perm(1:500);
+% templateh = vl_plotframe(f(:,sel)) ;
+% set(templateh,'color','y','linewidth',2) ;
