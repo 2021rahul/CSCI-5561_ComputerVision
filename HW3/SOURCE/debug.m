1875f633f0a7ed1774dc498282adfab5063f1cea
Rahul Ghosh <ghosh128@umn.edu>
2019-03-29 19:12:21 -0500

    add files

--- HW3/SOURCE/debug.m
+++ HW3/SOURCE/debug.m
@@ -54,3 +54,43 @@ label_test_pred = PredictSVM(feature_train, label_train, feature_test);
 % label_test_pred = sp_PredictSVM(feature_train, label_train, feature_test);
 confusion = confusionmat(label_test, label_test_pred);
 accuracy = (sum(label_test_pred==label_test)*100)/ size(label_test, 1);
+%%
+dirname = '../DATA/scene_classification_data';
+filename = fullfile(dirname, 'train.txt');
+train = readtable(filename,'Delimiter',' ', 'ReadVariableNames', false);
+
+filename = fullfile(dirname, 'test.txt');
+test = readtable(filename,'Delimiter',' ', 'ReadVariableNames', false);
+%%
+output_size = [16, 16];
+feature_train = zeros(size(train, 1), output_size(1)*output_size(2));
+[label_train, label_train_name] = grp2idx(train{:,1});
+for i=1:size(train)
+    img = imread(fullfile(dirname, strrep(train{i,2}{1},'\','/')));
+    feature_train(i,:) = GetTinyImage(img, output_size)';
+end
+
+
+feature_test = zeros(size(test, 1), output_size(1)*output_size(2));
+[label_test, label_test_name] = grp2idx(test{:,1});
+for i=1:size(test)
+    img = imread(fullfile(dirname, strrep(test{i,2}{1},'\','/')));
+    feature_test(i,:) = GetTinyImage(img, output_size)';
+end
+%%
+label_test_pred = PredictKNN(feature_train, label_train, feature_test, 10);
+confusion = confusionmat(label_test, label_test_pred, 'Order', label_test_name);
+accuracy = (sum(label_test_pred==label_test)*100)/ size(label_test, 1);
+%%
+i=1;
+img = train_image_cell{i};
+output_size = [16, 16];
+original_size = size(img);
+feature = imresize(img, output_size);
+feature = imresize(feature, original_size);
+figure();
+imshow(img);
+imwrite(img, '../RESULT/original.png');
+figure();
+imshow(feature);
+imwrite(img, '../RESULT/tiny.png');
